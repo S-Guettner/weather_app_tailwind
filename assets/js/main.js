f7cf25d5b05d7641d4c8be4c78a5cdb4ddf5f122
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-10 21:04:04 +0100

    push to create new branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 /* ========== input ========== */
-let city = document.getElementById("locationInput").value;
-
-
+let locationInput = document.getElementById("locationInput");
+let city;
+let search = () =>{
+    city = locationInput.value;
+    console.log(city);
+}
 
 /* ========== get data ========== */
+
+fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`)
+    .then(response => response.json())
+    .then(data => document.querySelector(".temperature").innerHTML = (data["current"].temp_c));
